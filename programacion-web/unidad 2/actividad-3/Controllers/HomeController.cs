7b10a2a0057af787ead3305b647f1cba7e97307d
Alondra Delgadillo <171G0576@rcarbonifera.tecnm.mx>
2021-02-02 12:47:30 -0600

    Proyectos realizados en séptimo semestre en la carrera ingeniería en sistemas computacionales.

---
+++ programacion-web/unidad 2/actividad-3/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+////
+using MVC2Unidad3Actividad.Models;
+
+namespace MVC2Unidad3Actividad.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancico.OrderBy(x => x.Nombre).ToList();
+            return View(villancico);
+        }
+
+        public IActionResult Villancico(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("index");
+            }
+
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancico.FirstOrDefault(x => x.Id == id);
+            if (villancico == null)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(villancico);
+            }
+        }
+    }
+}
